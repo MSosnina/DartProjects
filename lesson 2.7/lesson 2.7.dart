9615d59b3b5c98de05d88df8e2a0e0dc0bb3e5ba
MSosnina <34159155+MSosnina@users.noreply.github.com>
2022-08-05 22:31:32 +0700

    Add files via upload

---
+++ lesson 2.7/lesson 2.7.dart
@@ -0,0 +1,20 @@
+void main(){
+  int a = 4;
+  int b = 10;
+
+  bool c = a == b;
+  bool d = a != b;
+
+  print('$c');
+  print(d);
+
+  bool m = true;
+  bool n = false;
+
+  print(m&n);
+  print(m|n);
+  print(m&&n);
+  print(m||n);
+  print(m^n);
+  print(!m);
+}
