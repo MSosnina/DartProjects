08f0be8bfb329a09114016db49d6558c286b4226
MSosnina <34159155+MSosnina@users.noreply.github.com>
2022-08-07 11:42:31 +0700

    Add files via upload

---
+++ lesson 3.3/lesson 3.3.dart
@@ -0,0 +1,16 @@
+void main(){
+
+  int c = sum(5,6);
+  print(c);
+
+  void m = printHello();
+}
+
+sum(int a, int b){
+  return (a + b);
+}
+
+void printHello(){
+  print('hello');
+  return null;
+}
